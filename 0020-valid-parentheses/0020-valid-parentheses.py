2de292ea95dcfdd4ed2c72c7221ee7a54b982a5f
Murilok22 <129698733+Murilok22@users.noreply.github.com>
2026-08-19 18:30:26 -0300

    0020-VALID-PARENTHESES Stats: Time: 0 ms (100%), Space: 19.3 MB (62.89%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,40 @@
+#Estrutura de Pilha (Stack)
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # Se for Ã­mpar,Ã© invÃ¡lido
+        if len(s) % 2 != 0:
+            return False
+
+        # DicionÃ¡rio de pares (fechamento: abertura)
+        mapeamento = {')': '(', '}': '{', ']': '['}
+        pilha = []
+
+        for char in s:
+            if char in mapeamento:
+                # Ã um caractere de fechamento
+                # Pega o topo da pilha (ou um valor genÃ©rico se estiver vazia)
+                topo = pilha.pop() if pilha else '#' 
+                # pop para retirar o ultimo caractere da pilha
+                # o if para saber se esta ou nÃ£o vazia 
+                # o else '#' e para nÃ£o quebrar ao pegar a lista vazia 
+
+                if mapeamento[char] != topo:
+                    return False
+            else:
+                # Ã um caractere de abertura, adiciona na pilha
+                pilha.append(char)
+                
+        return len(pilha) == 0
+
+
+
+
+
+
+
+
+
+
+
+
+
